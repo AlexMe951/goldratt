6d2c7d3be4ca7b835a49c8ed2cad5483f2ee793f
Колинченко Александр Анатольевич (4047380) <kolinchenko@dev.vtb.ru>
2021-07-02 10:21:51 +0300

    Initial commit

---
+++ src/main/java/com/alexme951/goldratt/GoldrattApplication.java
@@ -0,0 +1,13 @@
+package com.alexme951.goldratt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldrattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldrattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexme951/goldratt/GoldrattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexme951.goldratt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldrattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
